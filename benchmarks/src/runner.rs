e6fece7784a005ed140341866d6514a7beb7c78a
Barak Biber <daaniv@gmail.com>
2024-11-11 13:13:10 +0200

    Moved run_task and its helpers to a new file

---
+++ benchmarks/src/runner.rs
@@ -0,0 +1,114 @@
+use std::{path::Path, sync::Arc, time::{Duration, Instant}};
+
+use ruse_object_graph::Cache;
+use ruse_ts_synthesizer::TsSynthesizer;
+use tokio_util::sync::CancellationToken;
+use tracing::{error, info};
+
+use crate::{config::BenchmarkConfig, results::BenchmarkResult, task};
+
+struct TimeoutError {}
+
+impl std::fmt::Display for TimeoutError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Timeout")
+    }
+}
+impl std::fmt::Debug for TimeoutError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("TimeoutError").finish()
+    }
+}
+
+impl std::error::Error for TimeoutError {}
+
+async fn run_synthesizer(
+    synthesizer: &mut TsSynthesizer,
+    result: &mut BenchmarkResult,
+    max_iterations: u32,
+    cancel_token: CancellationToken,
+) {
+    let start = Instant::now();
+    let mut found = None;
+    for _ in 0..max_iterations {
+        let iteration_start = Instant::now();
+        let res = tokio::select! {
+            _ = cancel_token.cancelled() => Err(TimeoutError {}),
+            v = synthesizer.run_iteration() => Ok(v)
+        };
+        let iteration_took = iteration_start.elapsed();
+        if let Err(e) = res {
+            result.error(&e);
+            return;
+        }
+        result.add_iteration(iteration_took, synthesizer.statistics());
+        if let Ok(Some(p)) = res {
+            found = Some(p);
+            break;
+        }
+    }
+    let took = start.elapsed();
+    result.finish(found, took, synthesizer.statistics());
+    info!(target: "ruse::runner", "Benchmark took {:.3}s", took.as_secs_f32());
+}
+
+fn get_tokio_runtime(bench_config: &BenchmarkConfig) -> tokio::runtime::Runtime {
+    let mut runtime_builder = if bench_config.multi_thread {
+        tokio::runtime::Builder::new_multi_thread()
+    } else {
+        tokio::runtime::Builder::new_current_thread()
+    };
+
+    runtime_builder.enable_all().build().unwrap()
+}
+
+pub fn run_task(path: &Path, cache: Arc<Cache>, bench_config: &BenchmarkConfig) -> BenchmarkResult {
+    let task_name = path.file_name().unwrap().to_str().unwrap();
+    let mut result = BenchmarkResult::new(task_name);
+
+    let task = match task::SnythesisTask::from_json_file(path, &cache) {
+        Ok(v) => v,
+        Err(e) => {
+            error!(target: "ruse::runner", "Failed to parse task for {}. {}", task_name, e);
+            result.error(&e);
+            return result;
+        }
+    };
+    let mut synthesizer = match task.get_synthesizer(&cache) {
+        Ok(v) => v,
+        Err(e) => {
+            error!(target: "ruse::runner", "Failed to get synthesizer for task {}. {}", task_name, e);
+            result.error(&e);
+            return result;
+        }
+    };
+    info!(target: "ruse::runner", "Running {}", task_name);
+
+    let runtime = get_tokio_runtime(bench_config);
+
+    runtime.block_on(async {
+        let cancel_token = synthesizer.get_cancel_token();
+        let timeout = tokio::time::timeout(
+            bench_config.timeout,
+            run_synthesizer(
+                &mut synthesizer,
+                &mut result,
+                bench_config.max_iterations,
+                cancel_token.child_token(),
+            ),
+        );
+        if let Err(e) = timeout.await {
+            error!(target: "ruse::runner", "Reached timeout");
+            cancel_token.cancel();
+            result.error(&e);
+            result.add_iteration(Duration::from_secs(0), synthesizer.statistics());
+            result.finish(None, bench_config.timeout, synthesizer.statistics());
+        }
+    });
+
+    if bench_config.print_inserted_programs {
+        synthesizer.print_all_programs()
+    }
+
+    result
+}
