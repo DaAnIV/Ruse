249d457c78756b9b3c2ebe47e20b19b72abb35ae
Barak Biber <daaniv@gmail.com>
2025-01-31 23:23:43 +0200

    Fixed Embedding algorithm

--- lib/synthesizer/src/embedding.rs
+++ lib/synthesizer/src/embedding.rs
@@ -1,11 +1,11 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
-use crate::context::{Context, ContextArray, ValuesMap, VariableName};
+use crate::context::{Context, ContextArray, VariableName};
 use itertools::{self, izip};
 use ruse_object_graph::{
     graph_equality,
     value::{ObjectValue, Value},
-    vobj, GraphIndex, GraphsMap, NodeIndex, ObjectGraph,
+    Cache, GraphIndex, GraphsMap, NodeIndex, ObjectGraph, RootName,
 };
 
 #[cfg(feature = "trace_embeddings")]
@@ -52,6 +52,55 @@ pub fn merge_context_arrays(
     ))
 }
 
+fn verify_matching_primitive_values(q_1: &Context, p_2: &Context) -> bool {
+    let mut variables = HashSet::new();
+    variables.extend(q_1.variable_names());
+    variables.extend(p_2.variable_names());
+    for var in variables {
+        match (q_1.get_var_value(var), p_2.get_var_value(var)) {
+            (Some(post_val_1), Some(pre_val_2)) => match (post_val_1, pre_val_2) {
+                (Value::Primitive(prim_1), Value::Primitive(prim_2)) => {
+                    if prim_1 != prim_2 {
+                        return false;
+                    }
+                }
+                (Value::Object(_), Value::Object(_)) => {
+                    continue;
+                }
+                (_, _) => return false,
+            },
+            _ => (),
+        }
+    }
+
+    return true;
+}
+
+fn context_graph_roots(ctx: &Context) -> HashMap<RootName, ObjectValue> {
+    let mut roots = HashMap::new();
+
+    for g in ctx.graphs_map.graphs() {
+        for (r, n) in g.roots() {
+            assert!(
+                roots
+                    .insert(
+                        r.clone(),
+                        ObjectValue {
+                            obj_type: g.obj_type(n).cloned().unwrap(),
+                            graph_id: g.id,
+                            node: *n
+                        }
+                    )
+                    .is_none(),
+                "Already seen root {}",
+                r
+            );
+        }
+    }
+
+    roots
+}
+
 pub(crate) fn merge_context(
     p_1: &Context,
     q_1: &Context,
@@ -64,10 +113,25 @@ pub(crate) fn merge_context(
     embeddings_trace!("p_2: {}", p_2);
     embeddings_trace!("q_2: {}", q_2);
 
+    if !verify_matching_primitive_values(q_1, p_2) {
+        return Err(());
+    }
+
     let mut p_1_hat = p_1.values.as_ref().clone();
     let mut q_2_hat = q_2.values.as_ref().clone();
     let mut p_1_map_hat = GraphsMap::default();
     let mut q_2_map_hat = GraphsMap::default();
+
+    for (n, v) in p_2.values.iter() {
+        if !p_1_hat.contains_key(n) {
+            p_1_hat.insert(n.clone(), v.clone());
+        }
+    }
+    for (n, v) in q_1.values.iter() {
+        if !q_2_hat.contains_key(n) {
+            q_2_hat.insert(n.clone(), v.clone());
+        }
+    }
 
     p_1_map_hat.add_static_graphs(&p_2.graphs_map);
     p_1_map_hat.add_static_graphs(&p_1.graphs_map);
@@ -77,45 +141,34 @@ pub(crate) fn merge_context(
     let mut p1_hat_nodes_matches = HashMap::new();
     let mut q2_hat_nodes_matches = HashMap::new();
 
+    let p_1_roots = context_graph_roots(p_1);
+    let q_1_roots = context_graph_roots(q_1);
+    let p_2_roots = context_graph_roots(p_2);
+    let q_2_roots = context_graph_roots(q_2);
+
     let mut intersection = Vec::new();
     let mut only_p_2 = Vec::new();
     let mut only_q_1 = Vec::new();
 
-    let mut variables = HashSet::new();
-    variables.extend(q_1.variables());
-    variables.extend(p_2.variables());
-
-    for var in variables {
-        match (q_1.get_var_value(var), p_2.get_var_value(var)) {
-            (None, Some(pre_val_2)) => match pre_val_2 {
-                Value::Primitive(_) => (),
-                Value::Object(o) => only_p_2.push((var, o.clone())),
-            },
-            (Some(post_val_1), None) => match post_val_1 {
-                Value::Primitive(_) => (),
-                Value::Object(o) => only_q_1.push((var, o.clone())),
-            },
-            (Some(post_val_1), Some(pre_val_2)) => match (post_val_1, pre_val_2) {
-                (Value::Primitive(prim_1), Value::Primitive(prim_2)) => {
-                    if prim_1 != prim_2 {
-                        return Err(());
-                    }
-                }
-                (Value::Object(o_1), Value::Object(o_2)) => {
-                    intersection.push((var, o_1.clone(), o_2.clone()))
-                }
-                (_, _) => return Err(()),
-            },
-            (None, None) => continue,
+    for (r, q_1_o) in &q_1_roots {
+        if let Some(p_2_o) = p_2_roots.get(r) {
+            intersection.push((r, q_1_o, p_2_o));
+        } else {
+            only_q_1.push((r, q_1_o));
+        }
+    }
+    for (r, p_2_o) in &p_2_roots {
+        if !q_1_roots.contains_key(r) {
+            only_p_2.push((r, p_2_o));
         }
     }
 
     embeddings_trace!(
         "intersection: [{}]",
-        intersection.iter().map(|x| x.0).join(","),
+        intersection.iter().map(|x| x.0).join(", "),
     );
-    embeddings_trace!("only_p_2: [{}]", only_p_2.iter().map(|x| x.0).join(","));
-    embeddings_trace!("only_q_1: [{}]", only_q_1.iter().map(|x| x.0).join(","));
+    embeddings_trace!("only_p_2: [{}]", only_p_2.iter().map(|x| x.0).join(", "));
+    embeddings_trace!("only_q_1: [{}]", only_q_1.iter().map(|x| x.0).join(", "));
 
     let new_nodes_1 = triplet_new_nodes(p_1, q_1);
     let new_nodes_2 = HashSet::default();
@@ -133,12 +186,11 @@ pub(crate) fn merge_context(
         return Err(());
     }
 
-    for (var, _, _) in &intersection {
-        if let Some(q_2_o) = q_2.get_var_value(var) {
+    for (var, _, _) in intersection {
+        if let Some(q_2_o) = q_2_roots.get(var) {
             embed_object_value(
                 var,
-                q_2_o.obj().unwrap(),
-                &mut q_2_hat,
+                q_2_o,
                 &mut q_2_map_hat,
                 q_2,
                 &new_nodes_2,
@@ -146,11 +198,10 @@ pub(crate) fn merge_context(
             );
         }
 
-        if let Some(p_1_o) = p_1.get_var_value(var) {
+        if let Some(p_1_o) = p_1_roots.get(var) {
             embed_object_value(
                 var,
-                p_1_o.obj().unwrap(),
-                &mut p_1_hat,
+                p_1_o,
                 &mut p_1_map_hat,
                 p_1,
                 &new_nodes_1,
@@ -159,21 +210,19 @@ pub(crate) fn merge_context(
         }
     }
 
-    for (var, o_2) in &only_p_2 {
+    for (var, o_2) in only_p_2 {
         embed_object_value(
             var,
             o_2,
-            &mut p_1_hat,
             &mut p_1_map_hat,
             p_2,
             &new_nodes_1,
             &mut p1_hat_nodes_matches,
         );
-        if let Some(q_o_2) = q_2.get_var_value(var) {
+        if let Some(q_o_2) = q_2_roots.get(var) {
             embed_object_value(
                 var,
-                q_o_2.obj().unwrap(),
-                &mut q_2_hat,
+                q_o_2,
                 &mut q_2_map_hat,
                 q_2,
                 &new_nodes_2,
@@ -181,16 +230,25 @@ pub(crate) fn merge_context(
             );
         }
     }
-    for (var, o_1) in &only_q_1 {
+    for (var, o_1) in only_q_1 {
         embed_object_value(
             var,
             o_1,
-            &mut q_2_hat,
             &mut q_2_map_hat,
             q_1,
             &new_nodes_2,
             &mut q2_hat_nodes_matches,
         );
+        if let Some(p_o_1) = p_1_roots.get(var) {
+            embed_object_value(
+                var,
+                p_o_1,
+                &mut p_1_map_hat,
+                p_1,
+                &new_nodes_1,
+                &mut p1_hat_nodes_matches,
+            );
+        }
     }
 
     p_1_map_hat.extend(&p_1.graphs_map);
@@ -215,7 +273,6 @@ fn triplet_new_nodes(p_ctx: &Context, q_ctx: &Context) -> HashSet<NodeIndex> {
 fn embed_object_value(
     var: &VariableName,
     obj_val: &ObjectValue,
-    values_hat: &mut ValuesMap,
     map_hat: &mut GraphsMap,
     old_ctx: &Context,
     new_nodes: &HashSet<NodeIndex>,
@@ -246,9 +303,10 @@ fn embed_object_value(
             (graph, matches.get(&obj_val.node).unwrap())
         }
     };
-    graph.set_as_root(var.clone(), new_var_value.1);
+    if var.as_str() != Cache::OUTPUT_ROOT_NAME {
+        graph.set_as_root(var.clone(), new_var_value.1);
+    }
     map_hat.insert_graph(graph.into());
-    values_hat.insert(var.clone(), vobj!(obj_val.obj_type().clone(), new_var_value.0, new_var_value.1));
 
     true
 }
