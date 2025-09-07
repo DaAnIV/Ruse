4f2571e9e01066bbda4775fad853cc99e0f43757
Barak Biber <daaniv@gmail.com>
2025-09-07 14:42:49 +0300

    Small change to the run_benchmarks_on script

--- scripts/run_benchmarks_on.sh
+++ scripts/run_benchmarks_on.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+rm -rf results/results
 
 RUST_BACKTRACE=1 ./target/profiling/Ruse run \
     --timeout 300 \
     --max-iterations 5 \
     --max-mutations 3 \
-    --max-sequence-size 10 \
+    --max-sequence-size 3 \
     --workers-count 64 \
     --output results/results \
     --log results/log.jsonl \
