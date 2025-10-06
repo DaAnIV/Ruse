52a282e53d145f329b0301ef3071f5ecc7ba8995
Barak Biber <daaniv@gmail.com>
2025-10-06 12:56:54 +0300

    Added docker files

---
+++ docker/build.sh
@@ -0,0 +1,2 @@
+docker build -f ./Dockerfile.sobeq --tag sobeq --ssh default .                                                                                                                                              
+docker build -f ./Dockerfile --tag ruse --ssh default .
