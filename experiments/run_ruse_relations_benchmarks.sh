dfd463bd36eeee6e558101eed2432213c6ddcbda
Barak Biber <daaniv@gmail.com>
2025-10-09 19:16:21 +0300

    Run the relations experiment with one worker

--- experiments/run_ruse_relations_benchmarks.sh
+++ experiments/run_ruse_relations_benchmarks.sh
@@ -15,19 +15,15 @@ BENCHMARKS=(
     -b ../tasks/benchmarks/new_ruse/relations/
 )
 
-
-for i in {1..4}; do
-    echo "Run #$i"
-    ../target/release/Ruse run \
-        -o results/${NAME}_results/run_${i} \
-        --log results/${NAME}_results/${NAME}_log_${i}.jsonl \
-        -t 3600 \
-        --workers-count 96 \
-        --max-iterations 6 \
-        --max-mutations 3 \
-        --max-sequence-size 3 \
-        --max-task-mem 100GiB \
-        "${BENCHMARKS[@]}"
-done
+../target/release/Ruse run \
+    -o results/${NAME}_results/run \
+    --log results/${NAME}_results/${NAME}_log.jsonl \
+    -t 3600 \
+    --workers-count 1 \
+    --max-iterations 6 \
+    --max-mutations 3 \
+    --max-sequence-size 3 \
+    --max-task-mem 100GiB \
+    "${BENCHMARKS[@]}"
 
 python3 ../scripts/merge_results.py --delete results/${NAME}_results/run_*
